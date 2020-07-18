9790dfda326f769851920df0b3be6dabe4342222
SmiteshP <smiteshpatil2010@gmail.com>
2020-07-18 15:46:12 +0530

    Add topological sorting algorithm

---
+++ topological_sort.cpp
@@ -0,0 +1,29 @@
+// vertex indexing expected from 0
+// input format adjacency list
+// output format {is order valid?, order}
+std::pair <bool, std::vector <int> > topological_sort(std::vector <std::vector <int> >& edges) {
+	int num_vertex = edges.size();
+	std::vector <int> in_degree(num_vertex);
+	for (std::vector <int>& children : edges) {
+		for (int child : children) {
+			++in_degree[child];
+		}
+	}
+	std::vector <int> tsort;
+	tsort.reserve(num_vertex);
+	std::queue <int> process_queue;
+	for (int i = 0; i < num_vertex; ++i) {
+		if (!in_degree[i]) process_queue.push(i);
+	}
+	while (process_queue.size() != 0) {
+		int curr_vertex = process_queue.front();
+		process_queue.pop();
+		tsort.emplace_back(curr_vertex);
+		for (int child : edges[curr_vertex]) {
+			--in_degree[child];
+			if (!in_degree[child]) process_queue.push(child);
+		}
+	}
+	return std::make_pair((tsort.size() == num_vertex), tsort);
+}
+
