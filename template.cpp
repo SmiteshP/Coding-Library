441d8fb6613e7f44c32b57083142264f76b4d8b3
SmiteshP <smiteshpatil2010@gmail.com>
2020-07-26 10:39:51 +0530

    Remove rarely used defines

--- template.cpp
+++ template.cpp
@@ -1,24 +1,14 @@
-// 
-
 #include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long
 #define pb push_back
-#define FOR(i, a, b) for(int i=(a);i<(b);i++)
-#define RFOR(i, a, b) for (int i=(a);i>(b);i--)
-#define REP(i, n) FOR(i, 0, n)
-#define RREP(i, n) RFOR(i, n, 0)
+#define eb emplace_back
+#define REP(i, n) for(int i=0;i<(n);++i)
 #define caset int ___T; cin >> ___T; while(___T--)
 #define F first
 #define S second
 #define all(x) x.begin(), x.end()
-#define clr(x) memset(x, 0, sizeof(x))
-#define get1(a) ll int a; cin>>a;
-#define get2(a,b) ll int a,b; cin>>a>>b;
-#define get3(a,b,c) ll int a,b,c; cin>>a>>b>>c;
-#define gets(s) string s; cin >> s;
-
 #define fast_IO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define endl "\n"
 
